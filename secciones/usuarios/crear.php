b0d941b12291c39397a6da9dfff405a9d35f0f4d
testDanilo <hekireki98@gmail.com>
2023-11-05 23:05:18 -0300

    parte visual de los formularios terminada

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,5 +1,40 @@
 <?php include("../../templates/header.php"); ?>
 
-Crear usuarios
+<br>
+
+<div class="card">
+    <div class="card-header">
+        Datos del usuarios 
+    </div>
+    <div class="card-body">
+        
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+          <label for="nombredelusuario" class="form-label">Nombre del usuario:</label>
+          <input type="text"
+            class="form-control" name="nombredelusuario" id="nombredelusuario" aria-describedby="helpId" placeholder="Nombre del usuario">
+        </div>
+
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password"
+            class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Escriba su contraseña">
+        </div>
+
+        <div class="mb-3">
+          <label for="correo" class="form-label">Correo:</label>
+          <input type="email"
+            class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="Escriba su Correo">
+        </div>
+
+    <button type="button" class="btn btn-success">Agregar</button>  
+    <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+
+    </form>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
     
 <?php include("../../templates/footer.php"); ?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,5 +1,43 @@
 <?php include("../../templates/header.php"); ?>
 
-Listar usuarios
+<br>
+
+<div class="card">
+    <div class="card-header">
+    <a name="" id="" class="btn btn-primary" 
+        href="crear.php" role="button">
+        Agregar usuarios</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre del usuario</th>
+                    <th scope="col">Contraseña</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">1</td>
+                    <td>Danilo Meza</td>
+                    <td>****</td>
+                    <td>meza@gmail.com</td>
+                    <td>
+                        <input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar">
+                        <input name="btnborrar" id="btnborrar" class="btn btn-danger" type="button" value="Borrar">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+</div>        
+    </div>
+</div>
+
+
+
     
-    <?php include("../../templates/footer.php"); ?>
+<?php include("../../templates/footer.php"); ?>
