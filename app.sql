0a35140766ff6c2af80e827608a8723f694946a6
testDanilo <hekireki98@gmail.com>
2023-11-06 15:18:52 -0300

    base de datos

---
+++ app.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-11-2023 a las 19:08:43
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_empleados`
+--
+
+CREATE TABLE `tbl_empleados` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `foto` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `cv` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `idpuesto` int(11) NOT NULL,
+  `fechadeingreso` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ucs2 COLLATE=ucs2_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_empleados`
+--
+
+INSERT INTO `tbl_empleados` (`id`, `nombres`, `apellidos`, `foto`, `cv`, `idpuesto`, `fechadeingreso`) VALUES
+(1, 'Danilo', 'Meza', 'foto.jpg', 'cv.pdf', 1, '2022-11-11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_puestos`
+--
+
+CREATE TABLE `tbl_puestos` (
+  `id` int(11) NOT NULL,
+  `nombredelpuesto` varchar(255) COLLATE ucs2_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ucs2 COLLATE=ucs2_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_puestos`
+--
+
+INSERT INTO `tbl_puestos` (`id`, `nombredelpuesto`) VALUES
+(1, 'Programador Jr.'),
+(2, 'Programador Sr.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuarios`
+--
+
+CREATE TABLE `tbl_usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `password` varchar(255) COLLATE ucs2_spanish_ci NOT NULL,
+  `correo` varchar(255) COLLATE ucs2_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ucs2 COLLATE=ucs2_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_usuarios`
+--
+
+INSERT INTO `tbl_usuarios` (`id`, `usuario`, `password`, `correo`) VALUES
+(1, 'Ezequiel', '12345', 'ezequiel@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_empleados`
+--
+ALTER TABLE `tbl_empleados`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idpuesto` (`idpuesto`);
+
+--
+-- Indices de la tabla `tbl_puestos`
+--
+ALTER TABLE `tbl_puestos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_empleados`
+--
+ALTER TABLE `tbl_empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_puestos`
+--
+ALTER TABLE `tbl_puestos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_empleados`
+--
+ALTER TABLE `tbl_empleados`
+  ADD CONSTRAINT `tbl_empleados_ibfk_1` FOREIGN KEY (`idpuesto`) REFERENCES `tbl_puestos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
